5c6b20051c54f2086200084114209d8ac3e42059
alkarps <alserkarpov@gmail.com>
2021-09-28 22:21:53 +0300

    Счетчики побед + фон

--- hw04/src/main/java/ru/alkarps/android/school2021/myapplication/MainActivity.kt
+++ hw04/src/main/java/ru/alkarps/android/school2021/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
+
 package ru.alkarps.android.school2021.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var winCounter = 0
+    private var defeatCounter = 0
+    private lateinit var winCounterText: TextView
+    private lateinit var winCounterImage: ImageView
+    private lateinit var defeatCounterText: TextView
+    private lateinit var defeatCounterImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        winCounterText = findViewById(R.id.win_counter_text)
+        winCounterText.text = winCounter.toString()
+        winCounterImage = findViewById(R.id.win_counter_image)
+        defeatCounterText = findViewById(R.id.defeat_counter_text)
+        defeatCounterText.text = defeatCounter.toString()
+        defeatCounterImage = findViewById(R.id.defeat_counter_image)
     }
 }
