088dc571b11021824d06ab214fea4746b5f213d2
alkarps <alserkarpov@gmail.com>
2021-10-03 20:19:33 +0300

    Предварительная разметка

---
+++ hw06/src/main/java/ru/alkarps/android/school2021/hw06/converter/ConverterActivity.kt
@@ -0,0 +1,12 @@
+package ru.alkarps.android.school2021.hw06.converter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.alkarps.android.school2021.hw06.R
+
+class ConverterActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.converter_activity)
+    }
+}
--- hw06/src/main/java/ru/alkarps/android/school2021/hw06/main/MainActivity.kt
+++ hw06/src/main/java/ru/alkarps/android/school2021/hw06/main/MainActivity.kt
@@ -1,11 +1,15 @@
-package ru.alkarps.android.school2021.hw06
+package ru.alkarps.android.school2021.hw06.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import ru.alkarps.android.school2021.hw06.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val convertersView = findViewById<RecyclerView>(R.id.converters_list)
     }
 }
