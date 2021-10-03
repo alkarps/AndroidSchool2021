0a207b5b5652b2bfa2468254e3c9ba8ed51668a3
alkarps <alserkarpov@gmail.com>
2021-10-03 21:39:21 +0300

    Первый экран

--- hw06/src/main/java/ru/alkarps/android/school2021/hw06/main/MainActivity.kt
+++ hw06/src/main/java/ru/alkarps/android/school2021/hw06/main/MainActivity.kt
@@ -1,15 +1,25 @@
 package ru.alkarps.android.school2021.hw06.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import ru.alkarps.android.school2021.hw06.R
+import ru.alkarps.android.school2021.hw06.repository.Repository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val repository = Repository()
         val convertersView = findViewById<RecyclerView>(R.id.converters_list)
+        convertersView.adapter = ListAdapter(repository.quantities)
+        convertersView.layoutManager?.scrollToPosition(Int.MAX_VALUE / 2)
+        val divider = DividerItemDecoration(this, RecyclerView.VERTICAL)
+        ResourcesCompat.getDrawable(resources, R.drawable.converters_list_divider, theme)?.apply {
+            divider.setDrawable(this)
+        }
+        convertersView.addItemDecoration(divider)
     }
 }
---
+++ hw06/src/main/java/ru/alkarps/android/school2021/hw06/repository/Repository.kt
@@ -0,0 +1,12 @@
+package ru.alkarps.android.school2021.hw06.repository
+
+import ru.alkarps.android.school2021.hw06.R
+import ru.alkarps.android.school2021.hw06.model.Quantity
+
+class Repository {
+    val quantities = listOf(
+        Quantity(R.string.speed),
+        Quantity(R.string.distance),
+        Quantity(R.string.volume)
+    )
+}
