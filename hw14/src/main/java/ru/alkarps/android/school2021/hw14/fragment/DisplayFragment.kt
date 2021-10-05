72f03410e4280529fd726295affc523d90efe142
alkarps <alserkarpov@gmail.com>
2021-10-05 20:17:39 +0300

    Фрагмент с картинкой

---
+++ hw14/src/main/java/ru/alkarps/android/school2021/hw14/fragment/DisplayFragment.kt
@@ -0,0 +1,6 @@
+package ru.alkarps.android.school2021.hw14.fragment
+
+import androidx.fragment.app.Fragment
+import ru.alkarps.android.school2021.hw14.R
+
+class DisplayFragment : Fragment(R.layout.display_fragment_layout)
