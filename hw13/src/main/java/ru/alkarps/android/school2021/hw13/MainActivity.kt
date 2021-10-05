2225dfa2505411355190ea019fe5b80843da20f3
alkarps <alserkarpov@gmail.com>
2021-10-05 11:44:53 +0300

    Поворот экрана

--- hw13/src/main/java/ru/alkarps/android/school2021/hw13/MainActivity.kt
+++ hw13/src/main/java/ru/alkarps/android/school2021/hw13/MainActivity.kt
@@ -17,25 +17,45 @@ class MainActivity : AppCompatActivity(), PublicApi {
         root = findViewById(R.id.main_root)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.containsKey(SHOW_TEXT_FRAGMENT_KEY) &&
+            savedInstanceState.getBoolean(SHOW_TEXT_FRAGMENT_KEY)
+        ) initTextFragment()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(SHOW_TEXT_FRAGMENT_KEY, textFragment != null)
+    }
+
     override fun showTextFragment() {
         if (textFragment == null) {
-            val text = supportFragmentManager.findFragmentById(R.id.edit_fragment) as EditFragment
-            val viewGroup = FragmentContainerView(this)
-            viewGroup.layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-            viewGroup.id = R.id.text_fragment_id
-            root.addView(viewGroup)
-
-            textFragment = TextFragment.newInstance(text.getText())
-            supportFragmentManager.beginTransaction()
-                .add(R.id.text_fragment_id, textFragment!!)
-                .commit()
+            initTextFragment()
         }
+    }
+
+    private fun initTextFragment() {
+        val text = supportFragmentManager.findFragmentById(R.id.edit_fragment) as EditFragment
+        val viewGroup = FragmentContainerView(this)
+        viewGroup.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        viewGroup.id = R.id.text_fragment_id
+        root.addView(viewGroup)
+
+        textFragment = TextFragment.newInstance(text.getText())
+        supportFragmentManager.beginTransaction()
+            .add(R.id.text_fragment_id, textFragment!!)
+            .commit()
     }
 
     override fun updateTextFragment(text: String) {
         textFragment?.setText(text)
+    }
+
+    companion object {
+        const val SHOW_TEXT_FRAGMENT_KEY = "showTextFragment"
     }
 }
