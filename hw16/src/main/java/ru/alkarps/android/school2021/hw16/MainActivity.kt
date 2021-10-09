a101c92e5ecd95438db433d8a2d711a912807720
alkarps <alserkarpov@gmail.com>
2021-10-09 12:40:01 +0300

    Пустой проект

---
+++ hw16/src/main/java/ru/alkarps/android/school2021/hw16/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.alkarps.android.school2021.hw16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
