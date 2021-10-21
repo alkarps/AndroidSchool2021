36c0da9bf2856da7a5e0764fff0622b7a2c9a00d
Karpov AS <ASergeKarpov@sberbank.ru>
2021-10-21 18:54:26 +0300

    Экстрактор кода

---
+++ hw21/src/main/java/ru/alkarps/android/school2021/hw21/code/RegexCodeExtractor.kt
@@ -0,0 +1,6 @@
+package ru.alkarps.android.school2021.hw21.code
+
+class RegexCodeExtractor : CodeExtractor {
+    private val regex = Regex("\\d?")
+    override fun getCode(rawText: String): String? = regex.find(rawText)?.value
+}
