42eda60e987409ce7fbdca485ea2d1c5bde2552c
Karpov AS <ASergeKarpov@sberbank.ru>
2021-10-21 18:00:41 +0300

    Пустой проект

---
+++ hw21/src/main/java/ru/alkarps/android/school2021/hw21/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.alkarps.android.school2021.hw21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
