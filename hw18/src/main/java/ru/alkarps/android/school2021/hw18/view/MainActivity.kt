f15a0f583cdec7349eb6bfc52ad38a23f4fd3012
Karpov AS <ASergeKarpov@sberbank.ru>
2021-10-22 17:31:08 +0300

    Предварительный дизайн

--- hw18/src/main/java/ru/alkarps/android/school2021/hw18/view/MainActivity.kt
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.alkarps.android.school2021.hw18
+package ru.alkarps.android.school2021.hw18.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.alkarps.android.school2021.hw18.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
