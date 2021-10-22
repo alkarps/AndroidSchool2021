5187bdb5bd36b2e1d0403cf546ec2c67dc7f41d5
alkarps <alserkarpov@gmail.com>
2021-10-22 08:43:47 +0300

    Реализация бродкаста

--- hw21/src/main/java/ru/alkarps/android/school2021/hw21/MainActivity.kt
+++ hw21/src/main/java/ru/alkarps/android/school2021/hw21/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.alkarps.android.school2021.hw21
 import android.content.IntentFilter
 import android.os.Bundle
 import android.provider.Telephony
+import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import ru.alkarps.android.school2021.hw21.broadcast.SmsTextBroadcast
@@ -18,7 +19,10 @@ class MainActivity : AppCompatActivity(), SmsTextListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val filter = IntentFilter().apply { addAction(Telephony.Sms.Intents.SMS_RECEIVED_ACTION) }
+        val filter = IntentFilter().apply {
+            priority = 99999999
+            addAction(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)
+        }
         smsBroadcastReceiver = SmsTextBroadcast(this)
         registerReceiver(smsBroadcastReceiver, filter)
 
@@ -32,6 +36,11 @@ class MainActivity : AppCompatActivity(), SmsTextListener {
     }
 
     override fun onGettingSmsText(text: String) {
+        Log.i(TAG, "getting sms text: $text")
         smsTextInput.setText(codeExtractor.getCode(text))
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
--- hw21/src/main/java/ru/alkarps/android/school2021/hw21/broadcast/SmsTextBroadcast.kt
+++ hw21/src/main/java/ru/alkarps/android/school2021/hw21/broadcast/SmsTextBroadcast.kt
@@ -3,14 +3,44 @@ package ru.alkarps.android.school2021.hw21.broadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.telephony.SmsMessage
+import android.util.Log
 import java.lang.ref.WeakReference
 
 /**
  * Броадкаст для получения текста пришедшей СМС
  */
-class SmsTextBroadcast(listener: SmsTextListener) : BroadcastReceiver() {
+class SmsTextBroadcast(
+    listener: SmsTextListener
+) : BroadcastReceiver() {
     private val listener = WeakReference(listener)
     override fun onReceive(context: Context?, intent: Intent?) {
-        val text = intent?.extras
+        try {
+            Log.i(TAG, "startGettingSms")
+            intent?.extras?.apply {
+                val format = getString(SMS_FORMAT)
+                (get(SMS_PDUS) as Array<ByteArray?>)
+                    .map { createSmsMessage(it, format) }
+                    .filterNotNull()
+                    .map { it.messageBody }
+                    .forEach { listener.get()?.onGettingSmsText(it) }
+            }
+        } catch (e: RuntimeException) {
+            Log.i(TAG, e.message, e)
+        }
+    }
+
+    private fun createSmsMessage(pdus: ByteArray?, format: String?): SmsMessage? {
+        return if (pdus == null) null
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (format == null) null else SmsMessage.createFromPdu(pdus, format)
+        } else SmsMessage.createFromPdu(pdus)
+    }
+
+    companion object {
+        private const val SMS_FORMAT = "format"
+        private const val SMS_PDUS = "pdus"
+        private const val TAG = "SmsTextBroadcast"
     }
 }
