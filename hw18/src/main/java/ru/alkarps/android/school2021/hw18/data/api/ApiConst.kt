735e97abfb8dc749814ca04b9f94d98739b9b109
Karpov AS <ASergeKarpov@sberbank.ru>
2021-10-24 10:34:12 +0300

    Константы для вызова API

---
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/data/api/ApiConst.kt
@@ -0,0 +1,9 @@
+package ru.alkarps.android.school2021.hw18.data.api
+
+object ApiConst {
+    const val KEY = "c59c90be-2dc7-4e31-ac3d-bddd10133aed"
+    private const val BASE_URL = "https://holidayapi.com/v1/"
+    const val HOLIDAYS_URL = "${BASE_URL}holidays"
+    const val LANGUAGES_URL = "${BASE_URL}languages"
+    const val COUNTRIES_URL = "${BASE_URL}countries"
+}
