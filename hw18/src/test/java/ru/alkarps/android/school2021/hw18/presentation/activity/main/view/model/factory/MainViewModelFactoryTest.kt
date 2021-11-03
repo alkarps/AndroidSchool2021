2f61d1e3bf88e56d1770874e6bde3085b717a71e
alkarps <alserkarpov@gmail.com>
2021-11-03 17:47:46 +0300

    fix unit tests

--- hw18/src/test/java/ru/alkarps/android/school2021/hw18/presentation/activity/main/view/model/factory/MainViewModelFactoryTest.kt
+++ hw18/src/test/java/ru/alkarps/android/school2021/hw18/presentation/activity/main/view/model/factory/MainViewModelFactoryTest.kt
@@ -1,5 +1,6 @@
 package ru.alkarps.android.school2021.hw18.presentation.activity.main.view.model.factory
 
+import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import ru.alkarps.android.school2021.hw18.presentation.activity.main.view.model.MainViewModel
@@ -7,6 +8,7 @@ import ru.alkarps.android.school2021.hw18.presentation.activity.main.view.model.
 class MainViewModelFactoryTest {
     @Test
     fun create() {
-        assertThat(MainViewModelFactory().create(MainViewModel::class.java)).isNotNull
+        assertThat(MainViewModelFactory(mockk(), mockk()).create(MainViewModel::class.java))
+            .isNotNull
     }
 }
