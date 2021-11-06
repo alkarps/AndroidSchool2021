2173df7b832b8dfc06daf355c55492fc1416ac40
alkarps <alserkarpov@gmail.com>
2021-11-06 12:37:02 +0300

    Получение текущего языка в бизнес-слое

--- hw18/src/main/java/ru/alkarps/android/school2021/hw18/domen/holiday/impl/ImplHolidayService.kt
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/domen/holiday/impl/ImplHolidayService.kt
@@ -2,6 +2,7 @@ package ru.alkarps.android.school2021.hw18.domen.holiday.impl
 
 import ru.alkarps.android.school2021.hw18.domen.holiday.HolidayClient
 import ru.alkarps.android.school2021.hw18.domen.holiday.HolidayService
+import ru.alkarps.android.school2021.hw18.domen.language.LanguageService
 import ru.alkarps.android.school2021.hw18.domen.model.Holiday
 import ru.alkarps.android.school2021.hw18.domen.model.Period
 import javax.inject.Inject
@@ -13,9 +14,10 @@ import javax.inject.Inject
  * @constructor Создает новую реализацию [HolidayService]
  */
 class ImplHolidayService @Inject constructor(
-    private val client: HolidayClient
+    private val client: HolidayClient,
+    private val language: LanguageService
 ) : HolidayService {
     override fun getHolidays(period: Period): List<Holiday> {
-        return client.getHoliday(period, "RU", "RU")
+        return client.getHoliday(period, language.getCurrentLanguage().code, "RU")
     }
 }
--- hw18/src/test/java/ru/alkarps/android/school2021/hw18/domen/holiday/impl/ImplHolidayServiceTest.kt
+++ hw18/src/test/java/ru/alkarps/android/school2021/hw18/domen/holiday/impl/ImplHolidayServiceTest.kt
@@ -8,25 +8,32 @@ import org.junit.Before
 import org.junit.Test
 import ru.alkarps.android.school2021.hw18.domen.holiday.HolidayClient
 import ru.alkarps.android.school2021.hw18.domen.holiday.HolidayService
+import ru.alkarps.android.school2021.hw18.domen.language.LanguageService
 import ru.alkarps.android.school2021.hw18.domen.model.Holiday
+import ru.alkarps.android.school2021.hw18.domen.model.Language
 import ru.alkarps.android.school2021.hw18.domen.model.Period
 
 class ImplHolidayServiceTest {
     private lateinit var client: HolidayClient
+    private lateinit var language: LanguageService
     private lateinit var testService: HolidayService
 
     @Before
     fun setUp() {
         client = mockk()
-        testService = ImplHolidayService(client)
+        language = mockk()
+        testService = ImplHolidayService(client, language)
     }
 
     @Test
     fun getHolidays_whenCall_thenReturnHoliday() {
+        val code = "Code"
         val period = Period(2020)
-        val holidays = listOf(Holiday("", "", "", "", true))
-        every { client.getHoliday(any(), any(), any()) } returns holidays
-        assertThat(testService.getHolidays(period)).isEqualTo(holidays)
-        verify { client.getHoliday(period, "RU", "RU") }
+        val expected = listOf(Holiday("", "", "", "", true))
+        every { language.getCurrentLanguage() } returns Language(code, "Name")
+        every { client.getHoliday(any(), any(), any()) } returns expected
+        assertThat(testService.getHolidays(period)).isEqualTo(expected)
+        verify { language.getCurrentLanguage() }
+        verify { client.getHoliday(period, code, "RU") }
     }
 }
