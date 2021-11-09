e6d87834f646b3c6fbfdfe82f7c8c586dee2bb53
alkarps <alserkarpov@gmail.com>
2021-11-09 23:54:35 +0300

    Вывод списка слов и их перевод

--- hw29/src/main/java/ru/alkarps/android/school2021/hw29/presentation/main/view/MainActivity.kt
+++ hw29/src/main/java/ru/alkarps/android/school2021/hw29/presentation/main/view/MainActivity.kt
@@ -1,11 +1,12 @@
 package ru.alkarps.android.school2021.hw29.presentation.main.view
 
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import ru.alkarps.android.school2021.hw29.databinding.ActivityMainBinding
+import ru.alkarps.android.school2021.hw29.presentation.main.view.adapter.DictionaryAdapter
 import ru.alkarps.android.school2021.hw29.presentation.main.view.model.DictionaryViewModel
 import ru.alkarps.android.school2021.hw29.presentation.main.view.model.factory.DictionaryViewModelFactory
 
@@ -19,17 +20,17 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this, DictionaryViewModelFactory(applicationContext))
             .get(DictionaryViewModel::class.java)
-//        viewModel.insertData()
+        viewModel.insertData()
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.loadDataRx()
         viewModel.dictionaryItems.observe(this) {
-            binding.words.text = TextUtils.join(",", it)
+            binding.words.adapter = DictionaryAdapter(it)
         }
         viewModel.errors.observe(this) {
-            Log.i("MainActivity", "Error", it)
+            Snackbar.make(binding.root, it.toString(), BaseTransientBottomBar.LENGTH_LONG).show()
         }
     }
 }
---
+++ hw29/src/main/java/ru/alkarps/android/school2021/hw29/presentation/main/view/adapter/DictionaryAdapter.kt
@@ -0,0 +1,35 @@
+package ru.alkarps.android.school2021.hw29.presentation.main.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.alkarps.android.school2021.hw29.R
+import ru.alkarps.android.school2021.hw29.databinding.DictionaryItemBinding
+import ru.alkarps.android.school2021.hw29.domen.models.DictionaryItem
+
+class DictionaryAdapter(
+    private val items: List<DictionaryItem>
+) : RecyclerView.Adapter<DictionaryAdapter.DictionaryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DictionaryViewHolder =
+        DictionaryViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.dictionary_item, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: DictionaryViewHolder, position: Int) {
+        holder.onBind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class DictionaryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = DictionaryItemBinding.bind(itemView)
+        fun onBind(di: DictionaryItem) {
+            binding.keyword.text = di.keyword
+            binding.translation.text = di.translation
+            binding.wordImage.visibility = GONE
+        }
+    }
+}
