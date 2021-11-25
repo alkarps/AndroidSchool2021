236af92164f108e14b6689a59eea29e8db0bd30b
alkarps <alserkarpov@gmail.com>
2021-11-25 08:39:46 +0300

    Интерактор событий

---
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/domen/event/EventRepository.kt
@@ -0,0 +1,30 @@
+package ru.alkarps.android.school2021.hw18.domen.event
+
+import ru.alkarps.android.school2021.hw18.domen.model.Event
+
+/**
+ * Репозиторий событий
+ */
+interface EventRepository {
+    /**
+     * Метод получения события на указанную дату
+     *
+     * @param date дата, на которую нужно вернуть события
+     * @return список событий на указанную дату
+     */
+    fun getAllByDate(date: String): List<Event>
+
+    /**
+     * Метод удаления события по айди
+     *
+     * @param uuid айди события
+     */
+    fun delete(uuid: String)
+
+    /**
+     * Метод создания нового или обновления старого события
+     *
+     * @param event событие
+     */
+    fun createOrUpdate(event: Event)
+}
---
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/domen/event/impl/ImplEventInteractor.kt
@@ -0,0 +1,27 @@
+package ru.alkarps.android.school2021.hw18.domen.event.impl
+
+import ru.alkarps.android.school2021.hw18.domen.event.EventInteractor
+import ru.alkarps.android.school2021.hw18.domen.event.EventRepository
+import ru.alkarps.android.school2021.hw18.domen.model.Event
+import javax.inject.Inject
+
+/**
+ * Реализация [EventInteractor]
+ *
+ * @property repository репозиторий для работы с событиями
+ */
+class ImplEventInteractor @Inject constructor(
+    private val repository: EventRepository
+) : EventInteractor {
+    override fun getAllByDate(date: String): List<Event> {
+        return repository.getAllByDate(date)
+    }
+
+    override fun delete(uuid: String) {
+        repository.delete(uuid)
+    }
+
+    override fun createOrUpdate(event: Event) {
+        repository.createOrUpdate(event)
+    }
+}
