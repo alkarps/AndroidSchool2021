655b6e292cf6092877abc1a2df52d866951da14f
Karpov AS <ASergeKarpov@sberbank.ru>
2021-11-25 13:51:05 +0300

    Реализация простого репозитория событий

---
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/data/event/ImplEventRepository.kt
@@ -0,0 +1,21 @@
+package ru.alkarps.android.school2021.hw18.data.event
+
+import ru.alkarps.android.school2021.hw18.domen.event.EventRepository
+import ru.alkarps.android.school2021.hw18.domen.model.Event
+import javax.inject.Inject
+
+class ImplEventRepository @Inject constructor() : EventRepository {
+    private val storage = mutableListOf<Event>()
+    override fun getAllByDate(date: String): List<Event> {
+        return storage.filter { it.date == date }
+    }
+
+    override fun delete(uuid: String) {
+        storage.find { it.uuid == uuid }?.apply { storage.remove(this) }
+    }
+
+    override fun createOrUpdate(event: Event) {
+        delete(event.uuid)
+        storage.add(event)
+    }
+}
