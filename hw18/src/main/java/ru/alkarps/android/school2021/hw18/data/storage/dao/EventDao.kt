a3c03c57ccb105664e3048a0d42e86320b818a88
alkarps <alserkarpov@gmail.com>
2021-11-26 22:06:36 +0300

    Сортировка событий по времени

--- hw18/src/main/java/ru/alkarps/android/school2021/hw18/data/storage/dao/EventDao.kt
+++ hw18/src/main/java/ru/alkarps/android/school2021/hw18/data/storage/dao/EventDao.kt
@@ -3,6 +3,7 @@ package ru.alkarps.android.school2021.hw18.data.storage.dao
 import androidx.room.*
 import ru.alkarps.android.school2021.hw18.data.storage.HolidayApiDbContract
 import ru.alkarps.android.school2021.hw18.data.storage.entity.EventEntity
+
 /**
  * DAO для работы с обытиями
  */
@@ -14,7 +15,7 @@ interface EventDao {
      * @param date дата события
      * @return список событий
      */
-    @Query("SELECT * FROM ${HolidayApiDbContract.Tables.EVENT} WHERE ${HolidayApiDbContract.Event.DATE} = :date")
+    @Query("SELECT * FROM ${HolidayApiDbContract.Tables.EVENT} WHERE ${HolidayApiDbContract.Event.DATE} = :date ORDER BY ${HolidayApiDbContract.Event.START_TIME}")
     fun getByDate(date: String): List<EventEntity>
 
     /**
